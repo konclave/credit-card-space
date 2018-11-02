18ed9f06d0a4f51ad59c08a815e7a2ba8c45ce53
Ivan Vasilyev <vianomore@gmail.com>
2018-11-02 23:18:56 +0300

    remove all files except index.html from gh-pages

--- jest.transform.js
+++
@@ -1,4 +0,0 @@
-var fs = require('fs');
-var babelConfig = JSON.parse(fs.readFileSync('./.babelrc', "utf8"));
-babelConfig.presets[0][1].modules = 'commonjs';
-module.exports = require('babel-jest').createTransformer(babelConfig);
